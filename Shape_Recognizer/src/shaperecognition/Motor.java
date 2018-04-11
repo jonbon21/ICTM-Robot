de4d56bcdaaaa03bfd49b85cb594f533ad503cd3
Lennert <lennert.deruyck@gmail.com>
2018-04-11 12:37:59 +0200

    X en Y as homing aangemaakt

--- Shape_Recognizer/src/shaperecognition/Motor.java
+++ Shape_Recognizer/src/shaperecognition/Motor.java
@@ -1,71 +1,54 @@
 package shaperecognition;
 
 import lejos.hardware.Power;
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.motor.*;
 import lejos.hardware.port.*;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.SampleProvider;
+
 import java.lang.Math.*;
 
 /* to do: if USsensor is used -> make second constructor with this sensor as parameter, and add extra condition in homing routine
  * double check or only the sensor? only the sensor is probably best*/
 
-public class Motor extends EV3LargeRegulatedMotor implements Power {
+public class Motor extends EV3LargeRegulatedMotor {
 	Boolean isHomed = false;
 	Boolean homingBusy = false;
 	String motorName = "unknownMotor";
-	int homingSpeed = 10;
-	double normalMotorCurrent = 0;
-	double maxMotorCurrentForHoming = 0;
-	double overcurrentLimitForHoming = 0.10;
+	int homingSpeed = 60;
 	double mmToDegreesConversion = 1;
 	
-	public Motor(Port inpPort, int inpHomingSpeed, double inpMMToDegreesConversion) {   //double inpNormalMotorCurrent, String inpMotorName,
+	public Motor(Port inpPort) {   //double inpNormalMotorCurrent, String inpMotorName,
 		super(inpPort);
 		//normalMotorCurrent = inpNormalMotorCurrent;
-		maxMotorCurrentForHoming = overcurrentLimitForHoming*normalMotorCurrent + normalMotorCurrent;
+		//maxMotorCurrentForHoming = overcurrentLimitForHoming*normalMotorCurrent + normalMotorCurrent;
 		//motorName = inpMotorName;
-		mmToDegreesConversion = inpMMToDegreesConversion;
-		homingSpeed = (int) (inpHomingSpeed*mmToDegreesConversion); //  degrees/s
+		//mmToDegreesConversion = inpMMToDegreesConversion;
+		//homingSpeed = (int) (inpHomingSpeed*mmToDegreesConversion); //  degrees/s
 	}
 
-	public void home() {
-		while (!isHomed) {
-			homingBusy = true;
-			System.out.println("Homing " + motorName + " IN PROGRESS");
-			this.setSpeed(homingSpeed);
-		    this.backward();
-		    
-		    if (this.getMotorCurrent() > maxMotorCurrentForHoming) {
-		    	this.stop();
-		    	System.out.println("Homing " + motorName + " DONE");
-		    	isHomed = true;
-	    		homingBusy = false;
-	    		this.resetTachoCount();
-		    }
-		}	
-	}
+	public void home(EV3TouchSensor eindeloop) {
+		SampleProvider sp = eindeloop.getTouchMode();
+		float[] sample = new float[sp.sampleSize()];
+		System.out.println("Homing " + motorName + " in progress");
+		isHomed=false;
 		
-	
-	@Override
-	public int getVoltageMilliVolt() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public float getVoltage() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public float getBatteryCurrent() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public float getMotorCurrent() {
-		return this.getMotorCurrent();
+		while (!isHomed) {
+		
+			homingBusy = true;
+		
+		    sp.fetchSample(sample, 0);
+      	  	this.setSpeed(homingSpeed);
+      	  	this.backward();
+      	  	
+	        if (sample[0] >0) {
+	        	 this.stop();
+	        	 isHomed=true;
+	        	 System.out.println("Homing " + motorName + " DONE");
+	         }
+		}
 	}
 
 }
--- Shape_Recognizer/src/shaperecognition/ShapeRecognition.java
+++ Shape_Recognizer/src/shaperecognition/ShapeRecognition.java
@@ -2,42 +2,42 @@ package shaperecognition;   //hallo maarten
 
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
+import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.port.*;
 
 public class ShapeRecognition {
+	public static void main(String[] args) {
 	
-	double xAxis_mmToDegreesConversion;
-	double yAxis_mmToDegreesConversion;
-	double zAxis_mmToDegreesConversion;
-	
-	double standardMaxMotorCurrent = 0;
-	int standardHomingSpeed =  0; //in mm/s
-	
-	
-	Motor motorAxisX = new Motor(MotorPort.A, standardHomingSpeed, xAxis_mmToDegreesConversion);
-	int maxX = 1000; 					//set manually
-	int maxY = 500;						//set manually
-	Coordinates mapLimits = new Coordinates(maxX, maxY);
-	XYMap map = new XYMap(mapLimits);
-	
-	
-	
-	
-	
-/*	
-	while (Button.ESCAPE.isUp()) {   //algemene while, programma blijft lopen zolang niet op escape wordt geduwd
-    
-		public static void main() {
-			Button.waitForAnyPress();   //soort startknop
+		Button.LEDPattern(4);
+		Sound.beepSequenceUp();
+		System.out.println("Press any key to start");
 		
-			if not homed   //routin om alle axes the homen INDIEN nog niet gehomed 
+		Button.waitForAnyPress();
+		
+		double xAxis_mmToDegreesConversion;
+		double yAxis_mmToDegreesConversion;
+		double zAxis_mmToDegreesConversion;
+	
+		double standardMaxMotorCurrent = 0;
+		int standardHomingSpeed =  0; //in mm/s
+	
+	
+		Motor motorX = new Motor(MotorPort.A);
+		Motor motorY = new Motor(MotorPort.B);
+		EV3TouchSensor eindeloopX = new EV3TouchSensor(SensorPort.S2);
+		EV3TouchSensor eindeloopY = new EV3TouchSensor(SensorPort.S3);
+		int maxX = 50; 					//set manually
+		int maxY = 50;						//set manually
+		Coordinates mapLimits = new Coordinates(maxX, maxY);
+		XYMap map = new XYMap(mapLimits);
+			motorX.home(eindeloopX);
+			motorY.home(eindeloopY);
 			
-			motorAxisX.home(); //homing routine van elke motor aanroepen
+			System.out.println("Press any key to start mapping");
+			Button.waitForAnyPress();
 			
-			if homed  //
 			
-			if sensorpos != encoderposition = not homed
-		}
-} */
+	}
+	
 }
